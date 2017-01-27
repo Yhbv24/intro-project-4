d4ba46218567afbb8150623ec802d3c065ff53cb
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-27 15:51:55 -0800

    Finish styling, fix small bugs that cause bad looking variable names to display under certain conditions

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,13 @@ Pizza.prototype.price = function() { // Builds price for pizza based on customer
   return this.price;
 };
 
+var sizeChecker = function(pizzaSize) { // Checks to see whether the customer picked a pizza size
+  if (!pizzaSize) {
+    alert("Please make sure to choose a size before moving forward.")
+    stopLoad; // Purposely causing error to prevent form from being submitted; error will not matter if user selects a size
+  };
+};
+
 //*****FRONT-END*****
 
 $(function() {
@@ -36,6 +43,8 @@ $(function() {
     var pizzaToppings = [];
     var pizzaSize = $("input[name=size]:checked").val();
 
+    sizeChecker(pizzaSize);
+
     $.each($("input[name=toppings]:checked"), function() { // Adds checked toppings to pizzaToppings array
       pizzaToppings.push($(this).val());
     });
@@ -43,16 +52,25 @@ $(function() {
     var newPizzaOrder = new Pizza(pizzaCrust, pizzaToppings, pizzaSize);
     var newPizzaPrice = newPizzaOrder.price();
 
-    $("#menu").fadeOut(500);
+    $("#menu").hide();
     $("#receipt_div").fadeIn(500);
 
-    $("#output_crust").append("<li>" + newPizzaOrder.crust + "</li>");
+    if (pizzaCrust === "gluten_free") {
+      $("#output_crust").append("<li>Gluten-Free</li>")
+    } else {
+      $("#output_crust").append("<li>" + newPizzaOrder.crust + "</li>")
+    };
+
     $("#output_size").append("<li>" + newPizzaOrder.size + "</li>");
 
-    newPizzaOrder.toppings.forEach(function(topping) { // Displays each topping in the array as its own LI
-      $("#output_toppings").append("<li>" + topping + "</li>");
-    });
+    if (newPizzaOrder.toppings.length === 0) {
+      $("#output_toppings").append("<li>You didn't choose any toppings.</li>");
+    } else {
+      newPizzaOrder.toppings.forEach(function(topping) { // Displays each topping in the array as its own LI
+        $("#output_toppings").append("<li>" + topping + "</li>");
+      });
+    };
 
-    $("#total_price").text("$" + newPizzaOrder.price); // Outputs total price of pizza selection
+    $("#total_price").text("$" + newPizzaOrder.price.toFixed(2)); // Outputs total price of pizza selection
   });
 });
