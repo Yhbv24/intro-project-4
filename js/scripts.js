2bcc9760a3b5820d2c8e77247d2dccfe085d91a4
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-01-27 12:54:52 -0800

    Add working form, working submit button

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,30 @@ function Pizza(crust, toppings, size) {
   this.crust = crust;
   this.toppings = toppings;
   this.size = size;
-  this.price = 0;
 };
 
 Pizza.prototype.price = function() { // Builds price for pizza based on customer's selections
-  if (pizzaSize === "small") {
-    this.price = 8;
-  } else if (pizzaSize === "medium") {
-    this.price = 11;
-  } else {
-    this.price = 14;
-  }
+  this.price = 0;
 
-  if (pizzaCrust = "gluten_free") { // Adds 50¢ for the gluten-free option
+  if (this.size === "small") {
+    this.price += 8
+  } else if (this.size === "medium") {
+    this.price += 11
+  } else if (this.size === "large") {
+    this.price += 14
+  };
+
+  if (this.crust === "gluten_free") { // Adds 50¢ for the gluten-free option
     this.price += .5
   };
 
-  for (var i = 1; i < this.toppings.length; i++) { // Checks how many toppings the pizza has, but skips the option for no toppings
-    this.price += .75 * [i]
-  };
+  this.price += .75 * this.toppings.length; // Adds 75¢ for each ingredient
 
-  return this.price
+  return this.price;
+};
+
+Pizza.prototype.capitalize = function() {
+  return this.crust.charAt(0).toUpperCase() + this.slice(1);
 };
 
 //*****FRONT-END*****
@@ -35,12 +38,17 @@ $(function() {
 
     var pizzaCrust = $("#crust_style").val();
     var pizzaToppings = [];
-    var pizzaSize = $("input[name=\"size\"]:checked");
+    var pizzaSize = $("input[name=size]:checked").val();
 
-    $.each($("input[name=\"toppings\"]:checked"), function() { // Adds checked toppings to pizzaToppings array
+    $.each($("input[name=toppings]:checked"), function() { // Adds checked toppings to pizzaToppings array
       pizzaToppings.push($(this).val());
     });
 
+    var newPizzaOrder = new Pizza(pizzaCrust, pizzaToppings, pizzaSize);
+    var newPizzaPrice = newPizzaOrder.price();
 
+    $("#menu").fadeOut(500);
+    $("#receipt_list").append("<li>" + newPizzaOrder.capitalize() + "</li>")
+    $("#receipt").fadeIn(500);
   });
 });
